1dbea66dd4bb950889a6aec398abab1749e6eba3
Maciej Najbar <maciek.najbar@gmail.com>
2018-12-13 14:17:26 +0100

    Phone number required.

--- app/src/main/java/com/gmail/najbar/maciek/candidateassessment/domain/Candidate.kt
+++ app/src/main/java/com/gmail/najbar/maciek/candidateassessment/domain/Candidate.kt
@@ -4,5 +4,6 @@ class Candidate(val fullName: String, val phoneNumber: String) {
 
     init {
         if (fullName.split(' ').size < 2) throw NotFullNameException()
+        if (phoneNumber.matches(Regex("\\d{9}")).not()) throw PhoneNumberMissingException()
     }
 }
