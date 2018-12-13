91ef3a7e1bc4f883f52d69497b6caf769da6842f
Maciej Najbar <maciek.najbar@gmail.com>
2018-12-13 14:15:30 +0100

    Full name required.

--- app/src/main/java/com/gmail/najbar/maciek/candidateassessment/domain/Candidate.kt
+++ app/src/main/java/com/gmail/najbar/maciek/candidateassessment/domain/Candidate.kt
@@ -1,4 +1,8 @@
 package com.gmail.najbar.maciek.candidateassessment.domain
 
 class Candidate(val fullName: String) {
+
+    init {
+        if (fullName.split(' ').size < 2) throw NotFullNameException()
+    }
 }
