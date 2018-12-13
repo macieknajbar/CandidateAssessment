b4a0e7cb3c0e047e08ae96d48fae17816ef05255
Maciej Najbar <maciek.najbar@gmail.com>
2018-12-13 15:49:18 +0100

    Refactoring to data class.

--- app/src/main/java/com/gmail/najbar/maciek/candidateassessment/domain/PhoneNumber.kt
+++ app/src/main/java/com/gmail/najbar/maciek/candidateassessment/domain/PhoneNumber.kt
@@ -2,23 +2,9 @@ package com.gmail.najbar.maciek.candidateassessment.domain
 
 import com.gmail.najbar.maciek.candidateassessment.domain.exceptions.IncorrectPhoneNumberException
 
-class PhoneNumber(val phoneNumber: String) {
+data class PhoneNumber(val phoneNumber: String) {
     init {
         if (phoneNumber.matches(Regex("^\\+\\d{11}\$")).not())
             throw IncorrectPhoneNumberException()
-    }
-
-    override fun hashCode(): Int {
-        return phoneNumber.hashCode()
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) {
-            return true
-        }
-        if (other is PhoneNumber) {
-            return phoneNumber == other.phoneNumber
-        }
-        return false
     }
 }
