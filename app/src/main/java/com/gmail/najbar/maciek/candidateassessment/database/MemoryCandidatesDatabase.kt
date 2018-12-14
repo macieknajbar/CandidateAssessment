3f730b47801339664678db23ab26f15873fe8552
Maciej Najbar <maciek.najbar@gmail.com>
2018-12-14 13:18:03 +0100

    Database adds new records

--- app/src/main/java/com/gmail/najbar/maciek/candidateassessment/database/MemoryCandidatesDatabase.kt
+++ app/src/main/java/com/gmail/najbar/maciek/candidateassessment/database/MemoryCandidatesDatabase.kt
@@ -1,5 +1,7 @@
 package com.gmail.najbar.maciek.candidateassessment.database
 
+import java.util.UUID
+
 object MemoryCandidatesDatabase {
     private val candidateTable = mutableMapOf<String, String>()
     private val phoneNumberTable = mutableMapOf<String, List<String>>()
@@ -7,10 +9,14 @@ object MemoryCandidatesDatabase {
     data class DbCandidate(val name: String, val phoneNumbers: List<String>)
 
     fun getAll(): Collection<DbCandidate> {
-        TODO()
+        return candidateTable.flatMap {
+            listOf(DbCandidate(it.value, phoneNumberTable[it.key] as List<String>))
+        }
     }
 
     fun update(name: String, vararg phoneNumbers: String) {
-
+        val id = UUID.randomUUID().toString()
+        candidateTable[id] = name
+        phoneNumberTable[id] = phoneNumbers.toList()
     }
 }
