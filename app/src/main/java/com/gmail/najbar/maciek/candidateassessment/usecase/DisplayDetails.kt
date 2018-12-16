f6bc9805b909524e4593fef776991bd246198630
Maciej Najbar <maciek.najbar@gmail.com>
2018-12-16 20:54:24 +0100

    Only defined candidates are handled

--- app/src/main/java/com/gmail/najbar/maciek/candidateassessment/usecase/DisplayDetails.kt
+++ app/src/main/java/com/gmail/najbar/maciek/candidateassessment/usecase/DisplayDetails.kt
@@ -1,18 +1,11 @@
 package com.gmail.najbar.maciek.candidateassessment.usecase
 
-import com.gmail.najbar.maciek.candidateassessment.repository.exception.NoSuchCandidateException
-import java.util.UUID
-
 class DisplayDetails(
         private val presenter: Candidate.DisplayDetails.Presenter,
         private val repositoryDisplayDetails: Candidate.DisplayDetails.Repository) : Candidate.DisplayDetails {
 
     override fun of(candidateId: String) {
-        try {
-            val candidate = repositoryDisplayDetails.about(candidateId)
-            presenter.present(Candidate.DisplayDetails.Candidate.from(candidate))
-        } catch (ignored: NoSuchCandidateException) {
-            presenter.present(Candidate.DisplayDetails.Candidate(UUID.randomUUID().toString(), "", listOf(), ""))
-        }
+        val candidate = repositoryDisplayDetails.about(candidateId)
+        presenter.present(Candidate.DisplayDetails.Candidate.from(candidate))
     }
 }
